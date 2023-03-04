cc1c11e3da11f64ba33ff845909e12714a5b6174
jai <jaigaur.imp210@gmail.com>
2023-03-04 20:17:35 +0530

    commit

---
+++ CountInversion.java
@@ -0,0 +1,17 @@
+public class CountInversion {
+    public long inversionCount(long arr[], long n){
+        long count =0;
+
+        
+
+        
+        return count;
+    }
+    public static void main(String[] args) {
+        CountInversion a = new CountInversion();
+        long n= 5;
+        long arr[] = {2, 4, 1, 3, 5};
+        long ans = a.inversionCount(arr, n);
+        System.out.println(ans);
+    }
+}
