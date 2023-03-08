b4d96825e5a42ae549a1bea5bd8fdf744752fc51
jai <jaigaur.imp210@gmail.com>
2023-03-08 19:11:33 +0530

    commit

---
+++ duplicatesInArray.java
@@ -0,0 +1,14 @@
+
+
+public class duplicatesInArray {
+    public static long[] productExceptSelf(int nums[], int n) 
+	{ 
+        // code here
+        long ans[] = new long[n];
+
+        return ans;
+	}
+    public static void main(String[] args) {
+        
+    }
+}
