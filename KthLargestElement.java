30ed66dbdce1e38d7a351ad110b2ae66ba4b9dbd
jai <jaigaur.imp210@gmail.com>
2023-03-15 18:35:12 +0530

    commit

---
+++ KthLargestElement.java
@@ -0,0 +1,48 @@
+public class KthLargestElement {
+    public int pivot(int []arr,int l,int h){
+        int piv=arr[h];
+        int j=l-1;
+        for(int i=l;i<h;i++){
+            if(arr[i]<piv){
+                j++;
+                int a=arr[i];
+                arr[i]=arr[j];
+                arr[j]=a;
+            }
+        }
+        j++;
+        int a=arr[h];
+        arr[h]=arr[j];
+        arr[j]=a;
+        return j;
+
+    }
+    public  int findKthLargest(int[] arr, int k) {
+        int l=0;
+        int n = arr.length+1;
+        int h=arr.length-1;
+        int size=arr.length;
+        int piv=pivot(arr, 0, h);
+        while(true){
+            if((h-piv+1)>k){
+                l=piv+1;
+                size=(h-l+1);
+                piv=pivot(arr, l, h);
+            }else if((h-piv+1)<k){
+                h=piv-1;
+                k=k-(size-(h-l+1));
+                size=(h-l+1);
+                piv=pivot(arr, l, h);
+            }else{
+                return arr[piv];
+            }
+        }
+    }
+    
+    public static void main (String[] args){
+        int arr[] = {3,2,1,5,6,4};
+        KthLargestElement a = new KthLargestElement();
+        int ans = a.findKthLargest(arr, 2);
+        System.out.println(ans);   
+    } 
+}
