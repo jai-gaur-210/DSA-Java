c9d8489b84d3ba78c9cdf9180c6ff188c705a5cd
jai <jaigaur.imp210@gmail.com>
2023-08-05 12:57:09 +0530

    j

---
+++ RotatedArray.java
@@ -0,0 +1,46 @@
+
+class RotatedArray
+{  
+    public int search(int[] nums, int target) {      
+        int start=0,end=nums.length-1;       
+        while(start<=end)
+        {
+            int mid=start+(end-start)/2;
+            if(target==nums[mid])
+            {
+                return mid;
+            }            
+            else if(nums[start]<=nums[mid])
+            {               
+                if(target>=nums[start] && target<nums[mid])
+                {
+                    end=mid-1;
+                }
+                else
+                {
+                    start=mid+1;
+                }
+            }			
+            else
+            {
+                if(target>nums[mid] && target<=nums[end])
+                {
+                    start=mid+1;
+                }
+                else
+                {
+                    end=mid-1;
+                }
+            }
+        }
+        return -1;
+    }   
+    public static void main (String[] args)
+    {
+        int arr[] = {4,5,6,7,0,1,2};
+        RotatedArray a = new RotatedArray();
+        int ans = a.search(arr , 2);
+        System.out.println(ans);   
+    }    
+}
+
